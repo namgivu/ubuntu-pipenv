519ef84a970f6bf9ff17df3b67fa0a5165ec4029
namgivu <namgivu@gmail.com>
2020-03-27 15:25:11 +0700

    update image push

--- docker-image-push.sh
+++ docker-image-push.sh
@@ -3,16 +3,22 @@ docstring='
               ./docker-image-push.sh
               ./docker-image-push.sh | tee ./tmp/log/docker-image-push.`date +%Y%m%d_%H%M%S_%N`
     u=namgivu ./docker-image-push.sh  # docker hub push to user/repo :u
+
+    u=namgivu UBUNTU_VERSION=16.04 PYTHON_VERSION=3.7 ./docker-image-push.sh
 '
 
 SH=$(cd `dirname $BASH_SOURCE` && pwd)  # SH aka SCRIPT_HOME the containing folder of this script
 
-IMAGE_NAME=${IMAGE_NAME:-'namgivu/ubuntu-1604-pipenv'}
-IMAGE_TAG=${IMAGE_TAG:-"$IMAGE_NAME:latest"}
+if [[ -z $UBUNTU_VERSION ]]; then echo 'Param :UBUNTU_VERSION is required in env variable'; exit 1; fi
+if [[ -z $PYTHON_VERSION ]]; then echo 'Param :PYTHON_VERSION is required in env variable'; exit 1; fi
+
+
+ IMAGE_TAG=${IMAGE_TAG:-"$UBUNTU_VERSION-$PYTHON_VERSION"}
+IMAGE_NAME=${IMAGE_NAME:-"namgivu/ubuntu-pipenv:$IMAGE_TAG"}
 
 echo; echo -e "--> Checking image $IMAGE_TAG exists"
     set -x  # print executed commands ON
-        docker image ls $IMAGE_NAME | grep $IMAGE_NAME --color=always
+        docker image ls | grep $IMAGE_TAG --color=always
         image_result=`echo $?`
     { set +x; } 2>/dev/null  # print executed commands OFF ref. https://stackoverflow.com/a/19226038/248616
 
@@ -35,8 +41,8 @@ set -e  # halt if error ON
         echo; echo "You may want to switch your repo :private first to get docker push working; then switch it back to :public"; echo
 
         #           #local image   #remote image
-        docker tag  $IMAGE_TAG     "$IMAGE_TAG"
-        docker push                "$IMAGE_TAG"
+        docker tag  $IMAGE_NAME    $IMAGE_NAME
+        docker push                $IMAGE_NAME
 
     { set +x; } 2>/dev/null  # print executed commands OFF ref. https://stackoverflow.com/a/19226038/248616
 set +e  # halt if error OFF
